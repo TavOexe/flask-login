6fe47d30f33cbd948829acbf13ab6328e00b0244
Gustavo <91002464+TavOexe@users.noreply.github.com>
2023-08-02 16:20:32 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,9 +5,9 @@ class DevelopmentConfig (Config):
     DEBUG = True
     DRIVER = 'ODBC Driver 17 for SQL Server'
     SERVER = 'gmanserver.database.windows.net'
-    DATABASE = 'gman'
-    uid = 'gmanuser'
-    pwd = '#Gc949194518'
+    DATABASE = '$NAME'
+    uid = '$USER'
+    pwd = '$PASS'
   
 
     connection_string = f'DRIVER={DRIVER};SERVER={SERVER};DATABASE={DATABASE};UID={uid};PWD={pwd}'
